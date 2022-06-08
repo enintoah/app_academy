d58214299e28eb1530b915c6d3aedfaede6f6b5a
Anthonie Lorsithong <anthoniecorps@gmail.com>
2022-06-08 12:17:46 -0700

    W8D4

---
+++ homework/W8/D4/timeout.js
@@ -0,0 +1,11 @@
+window.setTimeout(function() {
+	console.log('HAMMERTIME');
+}, 5000);
+
+function hammerTime(time) {
+	window.setTimeout(function () {
+		console.log(`${time} is HAMMER TIME`);
+	}, 5000);
+}
+
+
